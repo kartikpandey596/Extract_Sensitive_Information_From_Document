189de31144df76c9d0bb0d724fcbe3ab7facce67
Kartik Pandey <kartikpandey596@gmail.com>
2025-03-12 00:43:10 +0100

    push the changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, VERSION } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
     selector: 'my-app',
+    imports: [HeaderComponent],
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css','assets/css/*']
 })
 export class AppComponent {
   title = "Hello"
